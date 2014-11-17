90abdae676241c2b66c97e8267c8244a57ec6199
ryanwdav <ryanwdav@usc.edu>
2014-11-17 12:47:27 -0800

    Removed unused .php files.

--- Ohako/php/header.php
+++
@@ -1,8 +0,0 @@
-<?php Include '/home/dank420/ohako/php/hitCount.php'; ?>
-<link rel="stylesheet" type="text/css" href="/stylesheets/header.css">
-<div id="header">
-	<a href="/"><span id="title">Ohako</span></a>
-	<div id="header_user_div">
-		User Name
-	</div>
-</div>
