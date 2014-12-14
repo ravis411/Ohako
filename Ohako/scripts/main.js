ed7881851242f1bbc2b01c8694683fd3bcf5512f
ryanwdav <ryanwdav@usc.edu>
2014-12-14 04:16:57 -0800

    Added support for saving and retrieving messages...

--- Ohako/scripts/main.js
+++ Ohako/scripts/main.js
@@ -10,6 +10,7 @@ mail=false;
 function init(){
 	buttonsInit();
 	view = View.Home;
+	loadMailThreads();
 }
 
 // Initializes all of the buttons
@@ -24,7 +25,8 @@ function buttonsInit(){
 	$('#searchNav').on('click', function(){displayView(View.Search)});
 	$('#settingsNav').on('click', function(){displayView(View.Settings)});
 	$('.contentVenueImage').on('click', function(){displayView(View.Venue)});
-	$('.mailItem').on('click', function(){displayMail()});
+	$('#mailList').on('click', '.mailItem' , handleMailClick);
+	$('#mailMessageSubmitButton').on('click', saveMessage);
 }
 
 /*
@@ -68,13 +70,55 @@ function closeCurrentView()
 /*
 	Functions for messages
 */
-function displayMail(){
-	if (!mail){
-		$('#mailView').html("<img src=\"images/email.png\">");
-		mail=true;
+function loadMailThreads(){
+	$.post("/scripts/getThreads.php",{}, function(result){
+		$("#mailList").html(result);
+	});
+}
+
+
+function handleMailClick(e){
+	if( $(this).hasClass("selectedMailThread") ){
+		$('#mailMessagesDiv').html("Select a message from left.");
+		$(this).removeClass("selectedMailThread");
 	}
 	else{
-		$('#mailView').html("Select a message from left.");
-		mail=false;
+		$(".selectedMailThread").removeClass("selectedMailThread");
+		displayMail(this);
 	}
+
 }
+
+function displayMail(thread){
+		$(thread).addClass("selectedMailThread");
+		$.post("/scripts/getMessages.php",{thread:$(thread).html()}, function(result){
+			$("#mailMessagesDiv").html(result);
+		});
+}
+
+function saveMessage(){
+	var threadName = $(".selectedMailThread").html();
+	var messageText = $("#mailInputDiv input").val();
+	
+	if(threadName == undefined){
+		return;
+	}
+	$.post("/scripts/saveMessage.php",
+		{
+			thread:threadName,
+			message:messageText
+		},
+		function(data, status){
+			if(data == "Success"){
+				//alert("Test Successful.");
+			}else{
+				alert("Data: " + data + ". Success: " + status);
+			}
+		});
+}
+
+
+
+
+
+
