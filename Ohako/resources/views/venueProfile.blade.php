0c69f4a2eb7ca38d940ae275124af20845580c6c
kkthxby3 <shiffer@usc.edu>
2015-03-29 16:49:19 -0700

    image pathes in venueProfile

--- Ohako/resources/views/venueProfile.blade.php
+++ Ohako/resources/views/venueProfile.blade.php
@@ -11,32 +11,32 @@
 
 				<div id="venueDetails" >
 					<div id="venueTitle"> {{ $venue[0]['name'] }} </div> <br/>
-					<div id="drinks"> <img width="20px" height="20px" src="images/icons/drinks.png" alt="drinks"/> 
+					<div id="drinks"> <img width="20px" height="20px" src="/images/icons/drinks.png" alt="drinks"/> 
 						@if ($details[0]['drinks'] != "true")
-							<img id="prohibited" src="images/icons/prohibited.png" width="25px" height="25px" \>
+							<img id="prohibited" src="/images/icons/prohibited.png" width="25px" height="25px" \>
 						@endif
 					</div>
-					<div id="food"> <img width="20px" height="20px" src="images/icons/food.png" alt="food"/> 
+					<div id="food"> <img width="20px" height="20px" src="/images/icons/food.png" alt="food"/> 
 					@if ($details[0]['food'] != "true")
-							<img id="prohibited" src="images/icons/prohibited.png" width="25px" height="25px" \>
+							<img id="prohibited" src="/images/icons/prohibited.png" width="25px" height="25px" \>
 						@endif
 					</div>
-					<div id="patio"> <img width="20px" height="20px" src="images/icons/patio.png" alt="patio"/> 
+					<div id="patio"> <img width="20px" height="20px" src="/images/icons/patio.png" alt="patio"/> 
 					@if ($details[0]['patio'] != "true")
-							<img id="prohibited" src="images/icons/prohibited.png" width="25px" height="25px" \>
+							<img id="prohibited" src="/images/icons/prohibited.png" width="25px" height="25px" \>
 						@endif
 					</div>
-					<div id="smoking"> <img width="25px" height="30px" src="images/icons/smoking.png" alt="smoking" /> 
+					<div id="smoking"> <img width="25px" height="30px" src="/images/icons/smoking.png" alt="smoking" /> 
 						@if ($details[0]['smoking'] != "true")
-							<img id="prohibited" src="images/icons/prohibited.png" width="25px" height="25px" \>
+							<img id="prohibited" src="/images/icons/prohibited.png" width="25px" height="25px" \>
 						@endif
 					</div>
 					<br/> <br/>
-					<div id="karaokeListing"> <img id="karaokeIcon" width="20px" height="20px" src="images/icons/karaoke.png" alt="karaoke" /> 
+					<div id="karaokeListing"> <img id="karaokeIcon" width="20px" height="20px" src="/images/icons/karaoke.png" alt="karaoke" /> 
 						<div id="karaokeNights"> {{ $karaoke }} </div>  
 					</div>
 					<br/> 
-					<div id="locationListing"> <img id="locationIcon" width="20px" height="20px" src="images/icons/location.png" alt="location" /> 
+					<div id="locationListing"> <img id="locationIcon" width="20px" height="20px" src="/images/icons/location.png" alt="location" /> 
 						<div id="location"> {{ $location[0]['street'] }} </div> 
 					</div>
 				</div>
