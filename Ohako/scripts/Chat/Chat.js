ff903e8fd4b216304518045e1aedf62781d51523
ryanwdav <ryanwdav@usc.edu>
2015-05-11 17:38:29 -0700

    Added a new Chat class...still gotta clean up the html and such but its working. :D

---
+++ Ohako/scripts/Chat/Chat.js
@@ -0,0 +1,100 @@
+//Chat.js 
+//Client chat class
+var socketURL = "http://wrytek.us";
+var chat = null;
+
+//Params:
+//userID: username/userID
+//divID: the id of the div that will contain the chat.
+var Chat = function(userID, divID, room){
+
+	//Can only have one Chat
+	if(chat === null || chat === undefined){
+		chat = this;
+		console.log("Initializing sockets...");
+	}else return $(window).Chat;
+
+	this.room = room === undefined?"default":room;
+	this.socket = null;
+	if(divID === null || divID === undefined)	divID = "Chat";
+	this.$div = $("#" + divID);
+	this.$input = null;
+	this.$messages = null;
+	this.initSocket(userID);
+	this.initHTML();
+}
+
+
+// initSocket
+//
+Chat.prototype.initSocket = function(userID){
+	this.socket = io(socketURL + ":8080/chat/");
+	this.socket.emit('set user', userID);//Set the user id for this connection.
+
+	this.socket.on('chat message', function(data){
+		chat.addChatMessage(data);
+		console.log("chat message: " + data);
+	});
+
+	//Join and Leave room events
+	this.socket.on('joined room', function(data){
+		chat.addRoomJoinedMessage(data);
+	});
+	this.socket.on('left room', function(data){
+		chat.addRoomLeftMessage(data);
+	});
+
+	//Chat Typing Events
+	this.socket.on('typing', function(data){
+
+	});
+	this.socket.on('stop typing', function(data){
+
+	});
+
+
+}///End initSocket
+
+//Function addChatMessage
+// params: data {room: x, username: x}
+Chat.prototype.addChatMessage = function(data){
+	appendMessage(data.username + ": " + data.msg);
+}
+
+Chat.prototype.addRoomJoinedMessage = function(data){
+	appendMessage(data.username + " joined room " + data.room);
+}
+Chat.prototype.addRoomLeftMessage = function(data){
+	appendMessage(data.username + " left room " + data.room);
+}
+
+Chat.prototype.joinRoom = function(room){
+	this.socket.emit('joinRoom', room);
+}
+
+
+Chat.prototype.initHTML = function(){
+	
+
+	this.$input = $('<form class="chat_input_form" action=""><input class="messageInput" autocomplete="off" /><button>Send</button></form>');
+	this.$messages = $('<div class="Chat_messages_container"></div>');
+
+	this.$div.append(this.$messages);
+	this.$div.append(this.$input);
+
+	this.$div.on('submit', 'form', function(){
+		var msg = chat.$input.children('.messageInput').val();
+		var room = "all";
+		chat.socket.emit('chat message', {room: room, msg: msg});
+		chat.$input.children('.messageInput').val('');
+		console.log('Sending message: ', msg);
+		return false;
+  });
+}
+
+
+
+
+function appendMessage(message){
+	chat.$messages.append("<li>" + message + "</li>");
+}
---
+++ Ohako/scripts/Chat/index.js
@@ -0,0 +1,126 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/Chat.js', function(req, res){
+  res.sendFile(__dirname + '/Chat.js');
+});
+
+
+var sockets = [];
+var users = [];
+
+
+
+var chat = io.of('/chat/')
+.on('connection',function(socket){
+
+
+	socket.userData = new User(socket);
+	socket.userData.setUserID("ravis");
+	users.push(socket.userData);
+	console.log("new /chat/ connection: ", socket.id);
+
+
+
+	socket.on('set user', function(username){
+		console.log(socket.id, " is now ", username)
+		socket.userData.setUserID(username);
+	});
+
+
+	socket.on('chat message', function(data){
+		console.log('chat message: from: ', socket.userData.userID, data, " room: ", data.room, " msg: ", data.msg);
+		chat.to(data.room).emit('chat message', {username: socket.userData.userID, msg: data.msg});
+	});
+
+
+
+
+
+	socket.on('typing', function(){
+		socket.broadcast.emit('typing', {username: socket.userData.userID});
+	});
+	socket.on('stop typing', function(){
+		socket.broadcast.emit('stop typing', {username: socket.userData.userID})
+	});
+
+
+	socket.on('joinRoom', function(room){
+		console.log('\njoinRoom: ', room, ' already in: ', socket.rooms);
+		socket.join(room)
+		chat.to(room).emit('joined room', {username: socket.userData.userID, room: room});
+		socket.userData.joinRoom(room);
+	});
+
+	socket.on('leaveRoom', function(msg){
+		leaveRoom(socket, msg);
+	});
+
+
+
+
+	socket.on('disconnect', function(reason){
+		console.log(socket.userData.userID, ' disconnected: ', reason);
+
+		var user = users.indexOf(socket.userData);
+
+		for (var i = users[user].rooms.length - 1; i >= 0; i--) {
+			leaveRoom(socket, users[user].rooms[i])
+		};
+
+		delete users[user];
+		users.splice(user, 1);
+	});
+
+});
+
+
+
+function leaveRoom(socket, room){
+	console.log(socket.id, " leaving room ", room)
+	chat.to(room).emit('left room', {username: socket.userData.userID, room: room})
+	socket.leave(room)
+}
+
+
+
+
+
+var User = function(socket){
+	this.socket = socket;
+	this.userID = null;
+	this.rooms = socket.rooms.slice();
+	console.log("New User added: ", this.rooms)
+}
+User.prototype.joinRoom = function(room){
+	console.log("User joining room: ", this.rooms)
+	if(this.rooms.indexOf(room) != -1){
+		console.log("Duplicate room: ", this.rooms)
+	}else
+		this.rooms.push(room);
+}
+User.prototype.setUserID = function(id){
+	this.userID = id;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+	console.log("Path: ", io.path(), " ::chat: ");
+});
