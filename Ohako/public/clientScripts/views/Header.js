1a2a210864fa46417e1a08c9debf3ffd80bc3f11
kkthxby3 <shiffer@usc.edu>
2015-05-13 18:50:27 -0700

    Fix header

--- Ohako/public/clientScripts/views/Header.js
+++ Ohako/public/clientScripts/views/Header.js
@@ -29,7 +29,7 @@ function Header() {
 	this.checkOut = '<div id="checkIn">\
 						<div id="checkInInterior">\
 							<div id="checkInLogo">\
-								<img src="images/checkIn/901bar.png"/>\
+								<img src="images/901.jpg" width="100" height="80"/>\
 							</div>\
 	\
 							<div id="checkInDetails">\
